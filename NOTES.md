affd70f88bb46d111ba81a83ef3d1dcb71e832de
Ramz001 <rkenjaev1@gmail.com>
2026-01-03 19:25:54 +0500

    generating profiles controllers and modules

---
+++ NOTES.md
@@ -0,0 +1,14 @@
+1.
+
+generate a project
+
+`nest new project-name --directory`
+
+generate a module
+
+`nest generate module module-name`
+
+generate a controller
+
+`nest generate controller controller-name`
+
---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+
+// todo: implement profile controller methods
+// CRUD operations for user profiles + by id with params.
+
+@Controller('profile')
+export class ProfileController {}
