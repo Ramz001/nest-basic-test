1a38d170f2e9e1d050d007ec214975e096f854b3
Ramz001 <rkenjaev1@gmail.com>
2026-01-06 17:31:56 +0500

    refactor(prisma): streamline connection string handling in PrismaService constructor

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,13 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import 'dotenv/config';
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './generated/prisma/client';
-import 'dotenv/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
+    const connectionString = process.env.DATABASE_URL as string;
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not defined in environment variables');
+    }
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+      connectionString,
     });
     super({ adapter });
   }
